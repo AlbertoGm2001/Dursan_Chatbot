4c27fdb6e7bebf3b2291d7b2ab7d475e1e2f740b
BetoGMValum <agmartin@valumre.com>
2025-11-12 11:03:36 +0100

    Adapted image urls to read from s3

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -251,7 +251,7 @@ export default function CarChatApp(): JSX.Element {
       <div className="header">
         <div className="header-inner">
           <div className="header-logo">
-            <img src="images/dursan_logo.jpeg" alt="Dursan Logo" />
+            <img src="https://dursan-chatbot.s3.eu-west-1.amazonaws.com/dursan_logo.jpeg" alt="Dursan Logo" />
           </div>
           <div>
             <div className="header-title">DURSAN AI</div>
@@ -330,7 +330,10 @@ export default function CarChatApp(): JSX.Element {
                 <div key={rec.id} className="car-card">
                   {rec.image_url && (
                     <div className="car-image">
-                      <img src={`images/car_images/${rec.image_url}`} alt={`${rec.car_brand} ${rec.car_model}`} />
+                      <img
+                        src={`https://dursan-chatbot.s3.eu-west-1.amazonaws.com/scrap-images/${rec.image_url}`}
+                        alt={`${rec.car_brand} ${rec.car_model}`}
+                      />
                     </div>
                   )}
                   <a href={rec.url} target="_blank" rel="noopener noreferrer">
