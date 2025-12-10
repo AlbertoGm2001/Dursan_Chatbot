99e7361d8ac69ab7483041cd862e923c81b8bcdd
AlbertoGm2001 <alberto.garciam@hotmail.es>
2025-12-10 10:27:10 +0100

    Added fallback to 2.5 Flash if Lite model fails

--- api/modules/utils.py
+++ api/modules/utils.py
@@ -5,11 +5,22 @@ load_dotenv()
 
 client = genai.Client()
 def gemini_request(text):
-    response= client.models.generate_content(
-    model="gemini-2.5-flash-lite",   
-    contents=text
-    )
-    return response.text.strip()
+    try:
+        # First attempt with the lite model
+        response = client.models.generate_content(
+            model="gemini-2.5-flash-lite",
+            contents=text
+        )
+        return response.text.strip()
+
+    except Exception as e:
+        print(f"Lite model failed with error: {e}")
+        # Retry using the full flash model
+        response = client.models.generate_content(
+            model="gemini-2.5-flash",
+            contents=text
+        )
+        return response.text.strip()
 
 
 if __name__=="__main__":
